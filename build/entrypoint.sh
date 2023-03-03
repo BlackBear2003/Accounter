60d0a5719077d9eb5f42381b000f4521e97dd430
weizhile <348358584@qq.com>
2023-03-03 23:10:47 +0800

    docker

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,6 +4,8 @@ nohup java -jar /eureka.jar > eureka.log &
 nohup java -jar /gateway.jar > gateway.log &
 nohup java -jar /auth.jar > auth.log &
 nohup java -jar /api.jar > api.log &
+nohup java -jar /file.jar > file.log &
+nohup java -jar /ti.jar > ti.log &
 
 # 死循环，保持docker前台运行
 while [[ true ]]; do
